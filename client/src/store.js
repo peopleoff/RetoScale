ec917f9519cb0e873238e1c7e196dfaefff35e57
Ruslan Belyy <ruslanbelyy@gmail.com>
2019-02-20 21:55:43 -0500

    added images for guns and items locally

--- client/src/store.js
+++ client/src/store.js
@@ -5,15 +5,6 @@ import router from './router'
 
 Vue.use(Vuex)
 
-// const vuexLocalStorage = new VuexPersist({
-//   key: 'vuex', // The key to store the state on in the storage provider.
-//   storage: window.localStorage, // or window.sessionStorage or localForage
-//   // Function that passes the state and returns the state with only the objects you want to store.
-//   // reducer: state => state,
-//   // Function that passes a mutation and lets you decide if it should update the state in localStorage.
-//   // filter: mutation => (true)
-// })
-
 export default new Vuex.Store({
   state: {
     user: null,
